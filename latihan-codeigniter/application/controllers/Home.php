34a89c5c757955a917ace05cc3fe395d31ae4584
Saryanto1983 <yanclp83@gmail.com>
2020-03-14 11:53:26 +0700

    up

--- latihan-codeigniter/application/controllers/Home.php
+++ latihan-codeigniter/application/controllers/Home.php
@@ -3,9 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 	function index() {
-		//
+		//memanggil model dari folder HomeModel()
 		$this->load->model('HomeModel');
-		//
+		$this->load->view('Home');
+		//menampung fungsi dari getData()
 		$tampilkanData= $this->HomeModel->getData();
 
 		echo "Nama: ".$tampilkanData['Nama'] ."<br>";
---
+++ latihan-codeigniter/application/views/v_helperhtml.php
@@ -0,0 +1,12 @@
+<?php
+    echo heading('Hi!',3);
+    echo ul(array('Ini List 1','Ini List 2'));
+
+    $image_properties = array(
+        'src' => 'https://eventkampus.com/data/sekolah/168-1.jpeg',
+        'alt' => 'STMIK Jabar',
+        'width' => '100',
+        'height' => '100'
+    );
+
+    echo img($image_properties);
