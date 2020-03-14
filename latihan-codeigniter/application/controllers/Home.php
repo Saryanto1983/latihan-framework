41abec8d7ed8d420a2e7bb0c3bc24c88717a2b76
Saryanto1983 <yanclp83@gmail.com>
2020-03-14 11:12:17 +0700

    update 14-03-2020

--- latihan-codeigniter/application/controllers/Home.php
+++ latihan-codeigniter/application/controllers/Home.php
@@ -2,8 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
-	function index()
-	{
-		$this->load->view('Home');
+	function index() {
+		//
+		$this->load->model('HomeModel');
+		//
+		$tampilkanData= $this->HomeModel->getData();
+
+		echo "Nama: ".$tampilkanData['Nama'] ."<br>";
+		echo "Kampus: ".$tampilkanData['Kampus'] ."<br>";
+		echo "Website: ".$tampilkanData['Website'] ."<br>";
 	}
 }
